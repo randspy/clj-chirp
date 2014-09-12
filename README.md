c4064b94a90c6d126252a9d45e5b219fab319d48
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-09-12 20:20:39 +0200

    Program main loop added.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # clj-chirp
+
+Twitter console simulator.
+
+## Usage
+
+To run this application you need to execute
+
+    lein uberjar
+
+and 
+
+    java -jar target/clj-chirp-0.1.0-SNAPSHOT-standalone.jar
+
+To exit program write
+
+    exit
+    
+## License
+
+Copyright (C) 2014 Przemyslaw Koziel
+
+Distributed under the The MIT License.
--- src/clj_chirp/core.clj
+++ src/clj_chirp/core.clj
@@ -1,1 +1,16 @@
-(ns clj-chirp.core)
+(ns clj-chirp.core
+  (:gen-class))
+
+(defn exit-program? [user-input]
+  (= (.toUpperCase user-input) "EXIT"))
+
+(defn console-loop []
+  (loop []
+    (let [user-input (read-line)]
+      (if (exit-program? user-input)
+        (println "FINISHED")
+        (do (println user-input)
+            (recur))))))
+
+(defn -main [& args]
+  (console-loop))
