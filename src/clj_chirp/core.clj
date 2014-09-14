af6b2d2f978d79692bcfeddced8035475da512ca
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-09-14 16:04:46 +0200

    Posting usecase integrated into program logic.

--- src/clj_chirp/core.clj
+++ src/clj_chirp/core.clj
@@ -1,15 +1,23 @@
 (ns clj-chirp.core
+  (:require [clj-chirp.content :as content]
+            [clj-chirp.posting :as posting]
+            [clj-chirp.time :as time])
   (:gen-class))
 
 (defn exit-program? [user-input]
   (= (.toUpperCase user-input) "EXIT"))
+
+(def all-posts (atom []))
 
 (defn console-loop []
   (loop []
     (let [user-input (read-line)]
       (if (exit-program? user-input)
         (println "FINISHED")
-        (do (println user-input)
+        (do (let [user-data (content/split-user-from-content user-input "->")
+                  posts (posting/post @all-posts user-data (time/current-time))]
+              (reset! all-posts posts))
+            (println @all-posts)
             (recur))))))
 
 (defn -main [& args]
