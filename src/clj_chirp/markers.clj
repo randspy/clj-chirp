0e02d9b2bc70b84b1e856d248097599e103895fc
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-09-29 15:52:41 +0200

    Markers definition moved to a new file.

---
+++ src/clj_chirp/markers.clj
@@ -0,0 +1,9 @@
+(ns clj-chirp.markers)
+
+(def post-marker "->")
+
+(def follow-marker "follows")
+
+(def wall-marker "wall")
+
+(def all-markers [post-marker follow-marker wall-marker])
--- src/clj_chirp/usecases.clj
+++ src/clj_chirp/usecases.clj
@@ -2,6 +2,7 @@
   (:require [clj-chirp.content :as content]
             [clj-chirp.following :as following]
             [clj-chirp.formatter :as formatter]
+            [clj-chirp.markers :as markers]
             [clj-chirp.reading :as reading]
             [clj-chirp.posting :as posting]
             [clj-chirp.wall :as wall]
@@ -16,26 +17,26 @@
 (defn- first-marker-in-text? [text token]
   (let [tokens-with-their-position
         (remove nil?
-                (map #(token-with-position-in-text text %) ["->" "follows" "wall"]))]
+                (map #(token-with-position-in-text text %) markers/all-markers))]
     (= token (:token (first (sort-by :token-position tokens-with-their-position))))))
 
 (defn- when-posting [all-users-posts user-input timestamp]
-  (if (first-marker-in-text? user-input "->")
-    (let [user-input-after-split (content/split-user-name-from-content user-input "->")
+  (if (first-marker-in-text? user-input markers/post-marker)
+    (let [user-input-after-split (content/split-user-name-from-content user-input markers/post-marker)
           user-posts (posting/post @all-users-posts user-input-after-split timestamp)]
       (reset! all-users-posts user-posts))
     nil))
 
 (defn- when-following [all-users-posts user-input]
-  (if (first-marker-in-text? user-input "follows")
-    (let [user-input-after-split (content/split-user-name-from-content user-input "follows")
+  (if (first-marker-in-text? user-input markers/follow-marker)
+    (let [user-input-after-split (content/split-user-name-from-content user-input markers/follow-marker)
           user-posts (following/follows @all-users-posts user-input-after-split)]
       (reset! all-users-posts user-posts))
     nil))
 
 (defn- when-wall [all-users-posts user-input timestamp]
-  (if (first-marker-in-text? user-input "wall")
-    (let [user-name (:user-name (content/split-user-name-from-content user-input "wall"))
+  (if (first-marker-in-text? user-input markers/wall-marker)
+    (let [user-name (:user-name (content/split-user-name-from-content user-input markers/wall-marker))
           user-posts-with-follewed-ones (wall/show-wall @all-users-posts user-name)]
       (if (seq user-posts-with-follewed-ones)
         (print (formatter/formatted-posts
